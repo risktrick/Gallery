e79a8c253b1bba56a455cc7083b95202c14294a6
risktrick <risktrick@gmail.com>
2018-05-04 15:29:56 +0300

    does not work with cardview

--- app/src/main/java/company/wow/gallary/Repository.kt
+++ app/src/main/java/company/wow/gallary/Repository.kt
@@ -9,7 +9,7 @@ import io.reactivex.schedulers.Schedulers
 class Repository {
     fun getPhotos(): Observable<List<UnsplashModel>>? {
         val unsplashhApi = UnsplashhApi.Factory.create();
-        return unsplashhApi.search("39de03d8e03ddc8a583ee15227635acc30f176e6912566f49ba122fabd6829d4", 12, 100)
+        return unsplashhApi.search("39de03d8e03ddc8a583ee15227635acc30f176e6912566f49ba122fabd6829d4", 20, 50)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
 
