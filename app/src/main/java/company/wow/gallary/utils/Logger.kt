d3ea6a22f2d69fcce681f0ea3f04e2d1fb3ecee8
risktrick <risktrick@gmail.com>
2018-05-08 08:12:20 +0300

    refactor utils class

--- app/src/main/java/company/wow/gallary/utils/Logger.kt
+++ app/src/main/java/company/wow/gallary/utils/Logger.kt
@@ -3,12 +3,10 @@ package company.wow.gallary.utils
 import android.util.Log
 import company.wow.gallary.BuildConfig
 
-class Logger {
-    companion object {
-        fun log(s : String) {
-            if (BuildConfig.DEBUG) {
-                Log.e("aaa", s);
-            }
+object Logger {
+    fun log(s: String) {
+        if (BuildConfig.DEBUG) {
+            Log.e("aaa", s);
         }
     }
 }
